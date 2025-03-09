42402377aa17ffa6363aa90dee3e271afc4ccb6e
Only1cleve <cleve.king@snhu.edu>
2025-03-09 11:12:49 -0400

    Initial

---
+++ Source/GAM312KING/GAM312KING.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM312KING.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM312KING, "GAM312KING" );
